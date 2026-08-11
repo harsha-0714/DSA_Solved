948f82ce0415ded2ae5285fbbfcf157cd9e2b188
Harshavardhan <harshavardhand07@gmail.com>
2026-08-11 22:33:15 +0530

    Time: 0 ms (100%), Space: 42.1 MB (36.51%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int climbStairs(int n) {
+        int[] dp = new int[n+1];
+        Arrays.fill(dp,-1);
+        return memoFunc(n,dp);
+    }
+    private int memoFunc(int n , int[] dp){
+        if(n <= 2){
+            return n;
+        }
+        if(dp[n] != -1){
+            return dp[n];
+        }
+        return dp[n]= memoFunc(n-1,dp)+memoFunc(n-2,dp);
+    }
+}
