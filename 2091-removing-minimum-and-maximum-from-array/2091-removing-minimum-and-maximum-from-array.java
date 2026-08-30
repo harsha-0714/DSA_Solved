d4bab88c940406b1c7b6304c2bf11f0594c61979
Harshavardhan <harshavardhand07@gmail.com>
2026-08-30 10:21:36 +0530

    Time: 3 ms (61.82%), Space: 86.9 MB (23.09%) - LeetHub

--- 2091-removing-minimum-and-maximum-from-array/2091-removing-minimum-and-maximum-from-array.java
+++ 2091-removing-minimum-and-maximum-from-array/2091-removing-minimum-and-maximum-from-array.java
@@ -1,22 +1,17 @@
 class Solution {
     public int minimumDeletions(int[] nums) {
-        Map<Integer,Integer> map = new HashMap<>();
-        int max = Integer.MIN_VALUE , min = Integer.MAX_VALUE;
-        for(int i = 0;i<nums.length;i++){
-            map.put(nums[i],i);
-            max = Math.max(max,nums[i]);
-            min = Math.min(min,nums[i]);
+        int max = 0 , min = 0;
+        int n = nums.length;
+        if(n <= 2) return n;
+        for(int i = 1;i<n;i++){
+            if(nums[max] > nums[i]) max = i;
+            if(nums[min] < nums[i]) min = i;
         }
-        int n = nums.length , steps = 0;
-        steps = Math.max(map.get(max),map.get(min)) + 1;
-        steps = Math.min(steps , Math.max(n-map.get(max),n-map.get(min)));
-        int both = 0;
-        if(map.get(min) < map.get(max)){
-            both = map.get(min) + 1 + n - map.get(max);
-        }else{
-             both = map.get(max) + 1 + n - map.get(min);
-        }
-        steps = Math.min(steps,both);
+        int left = Math.min(max,min);
+        int right = Math.max(max,min);
+        int steps = right + 1;
+        steps = Math.min(steps,n - left);
+        steps = Math.min(steps , left + 1 +  n - right);
         return steps;
     }
 }
