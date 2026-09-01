7c42ea6bfee1bd6c73ed7d6a30c6db23f75914c6
Harshavardhan <harshavardhand07@gmail.com>
2026-09-01 22:33:23 +0530

    Time: 830 ms (10.8%), Space: 0B (100%) - LeetHub

---
+++ 1045-customers-who-bought-all-products/1045-customers-who-bought-all-products.sql
@@ -0,0 +1,5 @@
+# Write your MySQL query statement below
+select c.customer_id 
+from Customer c 
+group by c.customer_id
+having count(distinct c.product_key) = (select COUNT(product_key) from Product);
